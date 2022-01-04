f9d681a3097d052219e9126d1e46baae8fb76c3a
atu4403 <73111778+atu4403@users.noreply.github.com>
2022-01-04 16:43:39 +0900

    ✨ feat: add file_util.cat

--- src/adash/__init__.py
+++ src/adash/__init__.py
@@ -1,7 +1,7 @@
 """
 .. include:: ../../README.md
 """
-from .file_util import download, json_read, json_write
+from .file_util import download, json_read, json_write, cat
 from .string_util import (
     replace_all,
     to_half_string,
